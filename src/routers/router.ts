203b1af90eb0420a13e2a4c1c1735b6172fb67da
Ale13258 <aleja13258@gmail.ocm>
2024-06-12 22:45:45 -0500

    cambios 2.0

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -6,7 +6,7 @@ const router = Router();
 router.post('/puntaciones/create', registroController.crearRegistro);
 router.put('/puntaciones/intentos/:id', registroController.actualizarIntentos);
 router.get('/puntaciones/deportista/:deportistaId', registroController.obtenerRegistrosPorDeportistaId);
-router.post("/puntaciones/evento/:platform/:event/:partidaId", registroController.eventAction);
-router.get("/puntaciones/eventos/:platform/:partidaId", registroController.geteventsAction);
+router.post("/puntaciones/:platform/:event/:partidaId", registroController.eventAction);
+router.get("/puntaciones/:platform/:partidaId", registroController.geteventsAction);
 
 export default router;
